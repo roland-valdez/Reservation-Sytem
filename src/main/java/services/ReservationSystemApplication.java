37fabc18d865b1a09baad12a347d8b070f518395
Roland Valdez <valdez.roland.23@gmail.com>
2021-07-25 23:39:54 -0500

    working on index

--- src/main/java/services/ReservationSystemApplication.java
+++ src/main/java/services/ReservationSystemApplication.java
@@ -1,4 +1,4 @@
-package com.reservations.reservationsystem;
+package services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
